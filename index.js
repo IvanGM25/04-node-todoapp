2fb01a681ca6b2fae33db28efa23f5c5c1bb62c3
IvanGM25 <ivan-gabriel@live.com.ar>
2021-03-05 21:23:18 -0300

    Se creo la app de ToDo con dos items del menu: Crear tarea y Obtener todas las tareas

---
+++ index.js
@@ -0,0 +1,36 @@
+const {inquirerMenu} = require('./helpers/inquier');
+const {inquirerInput} = require('./helpers/inquier');
+const {TaskRepository} = require('./repositories/TaskRepository');
+
+const main = async()=>{
+    
+    const taskRepository = new TaskRepository();
+    let option = '';
+    do {
+        option = await inquirerMenu();
+        
+        
+        
+        switch (option) {
+            case 1:
+                const title = await inquirerInput('Task Title');
+                
+                taskRepository.createTask(title);
+                
+                
+        break;
+        
+        case 2:
+            const allTasks = taskRepository.getAllTask();
+            console.log(allTasks);
+            
+            
+            break;
+        }
+        
+    } while (option !== 0);
+        
+        
+}
+
+main();
